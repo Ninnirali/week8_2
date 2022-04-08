2e9866bb966e09c1ca0a235bd492b339c8d713a1
Ninnirali <connectwithnirali@gmail.com>
2022-04-08 23:35:06 +0100

    week8.2

---
+++ src/week8_2/Program_8_SysmbolTriangle.java
@@ -0,0 +1,33 @@
+package week8_2;
+
+import java.util.Scanner;
+
+/**
+ * Display right angle triangle of @ using nested for loops
+ * @
+ * @@
+ * @@@
+ * @@@@
+ * @@@@@
+ */
+public class Program_8_SysmbolTriangle {
+    public static void symbolTriangle(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("@");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of rows: ");
+        symbolTriangle(scanner.nextInt());
+        // closing the scanner object
+        scanner.close();
+    }
+
+
+}
